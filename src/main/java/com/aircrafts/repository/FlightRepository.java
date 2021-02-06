b470027a16a79ca88c4c1618e2b83a7f122c46f6
juablaz <jc-blazquez@hotmail.com>
2021-02-06 20:39:44 +0100

    ordenado descendente

--- src/main/java/com/aircrafts/repository/FlightRepository.java
+++ src/main/java/com/aircrafts/repository/FlightRepository.java
@@ -16,7 +16,7 @@ public interface FlightRepository extends JpaRepository<Flight, Long> {
       + "INNER JOIN airport a on f.destination_id = a.id AND a.city = :city "
       + "AND DATE_ADD(f.departure_date, INTERVAL duration HOUR) >= :arrivalDate "
       + "AND DATE_ADD(f.departure_date, INTERVAL duration HOUR) < DATE_ADD(:arrivalDate, INTERVAL duration DAY) "
-      + "ORDER BY DATE_ADD(f.departure_date, INTERVAL duration HOUR)", nativeQuery = true)
+      + "ORDER BY DATE_ADD(f.departure_date, INTERVAL duration HOUR) DESC ", nativeQuery = true)
   List<Flight> findAllFlightsByCityAndArrivalDate(@Param("city") String city, @Param("arrivalDate") Date arrivalDate);
 
 
