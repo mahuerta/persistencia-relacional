526c53a32a045e891aad95696b221e8cfecfeed1
juablaz <jc-blazquez@hotmail.com>
2021-02-06 20:40:03 +0100

    ordenado descendente

--- src/main/java/com/aircrafts/repository/PlaneRepository.java
+++ src/main/java/com/aircrafts/repository/PlaneRepository.java
@@ -1,6 +1,7 @@
 package com.aircrafts.repository;
 
 import com.aircrafts.model.Plane;
+import com.aircrafts.model.dto.PlaneDto;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
