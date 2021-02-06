9324b08880a7be92b2822f0b0cfeed53ea699448
juablaz <jc-blazquez@hotmail.com>
2021-02-06 11:00:18 +0100

    Estructura inicial de la BBDD

--- src/main/java/com/aircrafts/models/Company.java
+++ src/main/java/com/aircrafts/models/Company.java
@@ -1,8 +1,11 @@
 package com.aircrafts.models;
 
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -16,4 +19,10 @@ public class Company {
   private Long id;
 
   String name;
+
+  @OneToMany(cascade= CascadeType.ALL, mappedBy="company")
+  List<Crew> crews;
+
+  @OneToMany(cascade= CascadeType.ALL, mappedBy="company")
+  List<Mechanic> mechanics;
 }
