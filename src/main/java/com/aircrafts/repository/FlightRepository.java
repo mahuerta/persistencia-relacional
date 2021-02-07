93fbb77f9146dd5d99f07d6da6abaf197eb857c8
mahuerta <miguel5692@gmail.com>
2021-02-07 13:45:11 +0100

    Add fix on Query2

--- src/main/java/com/aircrafts/repository/FlightRepository.java
+++ src/main/java/com/aircrafts/repository/FlightRepository.java
@@ -14,7 +14,7 @@ public interface FlightRepository extends JpaRepository<Flight, Long> {
   @Query(value = "SELECT f.* FROM flight f "
       + "INNER JOIN airport a on f.destination_id = a.id AND a.city = :city "
       + "AND DATE_ADD(f.departure_date, INTERVAL duration HOUR) >= :arrivalDate "
-      + "AND DATE_ADD(f.departure_date, INTERVAL duration HOUR) < DATE_ADD(:arrivalDate, INTERVAL duration DAY) "
+      + "AND DATE_ADD(f.departure_date, INTERVAL duration HOUR) < DATE_ADD(:arrivalDate, INTERVAL 1 DAY) "
       + "ORDER BY DATE_ADD(f.departure_date, INTERVAL duration HOUR) DESC ", nativeQuery = true)
   List<Flight> findAllFlightsByCityAndArrivalDate(@Param("city") String city, @Param("arrivalDate") Date arrivalDate);
 
